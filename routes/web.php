057f32113e3463b1de5d182dd480c224cf00cf48
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2019-03-20 00:01:09 +0200

    Add Route path

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->namespace('Admin')->name('admin.')->group(function(){
+	Route::resource('/admin', 'HomeController')->only('index');
+	Route::resource('/admin/category', 'CategoryController');
+});
