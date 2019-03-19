9089a302d6661d537983555d1dcfd3695caa34c9
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2019-03-20 00:46:57 +0200

    Layout template

---
+++ resources/views/layouts/index_admin.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@section('title')Laravel@show</title>
+
+        @section('css')
+            include('layouts.css.css')
+        @show
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @section('header')
+                include('layout.header.index')
+            @show
+
+            @yield('content')
+        </div>
+
+        @section('footer')
+            include('layouts.footer.index')
+        @show
+
+        @section('js')
+            include('layouts.js.js');
+        @show
+    </body>
+</html>
